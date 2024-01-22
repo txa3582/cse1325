d84c20eecd5c77d8c3860873cbd38e26d798b9eb
“Thomas <txa3582@mavs.uta.edu>
2024-01-22 13:48:53 -0600

    extrem_bonus

---
+++ P01/extreme_bonus/Hello.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Hello 
+{
+    public static void main(String[] args) 
+    {
+       	String username = System.getProperty("user.name");
+        System.out.println("hello, " + username);
+    }
+}
