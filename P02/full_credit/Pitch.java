50e2895ee0d479a663f5ad93995f0f16b2cfa3b4
“Thomas <txa3582@mavs.uta.edu>
2024-01-23 12:02:13 -0600

    class and constructor

--- P02/full_credit/Pitch.java
+++ P02/full_credit/Pitch.java
@@ -1,4 +1,24 @@
-public enum 
+public class Note
 {
-	C, Db, D, Eb, E, F, Gb, G, Ab, A, B, B s
-} 
+	//field1: pitch enum
+	private enum Pitch
+	{
+		C, Db, D, Eb, E, F, Gb, G, Ab, A, B, B
+	}
+	private Pitch pitch;
+	//field2: octave
+	private int octave;
+
+	//contructor
+	public Note()
+	{
+		pitch = Pitch.C;
+		octave  = 0;
+
+		if(-5 >= octave || octave  >= 4)
+			throw IllegalArguementException
+				("octave must be between -5 and 4");
+	}
+
+}
+
