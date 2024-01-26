1669e04a4e2c359444012284b6868f92d8ec3916
“Thomas <txa3582@mavs.uta.edu>
2024-01-25 19:00:21 -0600

    constructor parameters

--- P02/bonus/Pitch.java
+++ P02/bonus/Pitch.java
@@ -1,10 +1,20 @@
 public enum Pitch
 {
-	C, Db, D, Eb, E, F, Gb, G, Ab, A, Bb, B;
+	C("C"), Db("D♭"), D("D"), Eb("E♭"), E("E"), F("F"), Gb("G♭"), G("G"), Ab("A♭"), A("A"), Bb("B♭"), B("B");
 
 	//field
 	private String pitch;
 	//constructor
+	private Pitch(String pitch)
+	{
+		this.pitch = pitch;	
+	}
+	//methods
+	@Override
+	public String toString()
+	{
+		return pitch;
+	}
 
 		 
 }
