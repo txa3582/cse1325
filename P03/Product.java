8ea075e0135d0e9b4229d4f9060b530e24ebac5b
“Thomas <txa3582@mavs.uta.edu>
2024-01-30 14:52:36 -0600

    Product setup

--- P03/Product.java
+++ P03/Product.java
@@ -1,4 +1,26 @@
-public class Product()
+public class Product
 {
-	 
+	//constructors
+	public Product(String name, int price)
+	{
+
+	}
+
+	public int getstockNumber()
+	{
+	}
+	public int getPrice()
+	{
+	}
+	//methods
+	@Override
+	public String toString()
+	{
+
+	}
+	//fields
+	private int stockNumber;
+	private String name;
+	private int price;
+
 }
