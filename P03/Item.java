5bb6faf0eea79b6ce2d4b86c62149ce81db06027
“Thomas <txa3582@mavs.uta.edu>
2024-01-30 14:58:21 -0600

    Item setup

--- P03/Item.java
+++ P03/Item.java
@@ -1,3 +1,20 @@
-public class Item()
+public class Item
 {
+	public Item(Product product, int price)
+	{
+	}
+	public int getstockNumber()
+	{
+	}
+	public int getPrice()
+	{
+	}
+	@Override
+	public String toString()
+	{
+	}
+	//fields
+	private Product product;
+	private int Quantity;
+	
 }
