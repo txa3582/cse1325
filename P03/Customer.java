a4767d32cd7d15b7a2e8bcd194ae12ecbd10fa5f
“Thomas <txa3582@mavs.uta.edu>
2024-01-30 14:46:29 -0600

    Customer setup

--- P03/Customer.java
+++ P03/Customer.java
@@ -1,3 +1,30 @@
-public class Customer()
+public class Customer
 {
+	//constructors
+	public Customer(String name, String email)
+	{
+		this.name = name;
+		this.email = email;
+
+	}
+
+	public Customer(BufferedReader br)
+	{
+
+	}
+
+	public save(BufferedWriter bw)
+	{
+
+	}
+
+	@Override
+	public String toString()
+	{
+
+	}
+
+	// fields
+	private String name;
+	private String email;
 }
