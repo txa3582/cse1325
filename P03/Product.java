469cb6965f427b8cf854d43d7c5f834c5d6fe697
“Thomas <txa3582@mavs.uta.edu>
2024-02-01 13:56:47 -0600

    price validation

--- P03/Product.java
+++ P03/Product.java
@@ -3,11 +3,16 @@ public class Product
 	//constructors
 	public Product(String name, int price)
 	{
+		this.name = name;
+		this.price = price;
+		//validate price
+		if(price < 0) throw new IllegalArgumentException("Invalid price " + name + ":" + price);
 
 	}
 
 	public int getstockNumber()
 	{
+	
 	}
 	public int getPrice()
 	{
@@ -19,7 +24,7 @@ public class Product
 
 	}
 	//fields
-	private int stockNumber;
+	private int nextstockNumber;
 	private String name;
 	private int price;
 
