b58a4d39ab37c1b653a03f0db8fc71a26b5ab60c
“Thomas <txa3582@mavs.uta.edu>
2024-02-01 19:24:10 -0600

    product update including string format

--- P03/Product.java
+++ P03/Product.java
@@ -3,30 +3,40 @@ public class Product
 	//constructors
 	public Product(String name, int price)
 	{
-		this.name = name;
-		this.price = price;
 		//validate price
 		if(price < 0) throw new IllegalArgumentException("Invalid price " + name + ":" + price);
 
+		nextStockNumber = (int) (100000000000.0 * Math.random()); 
+
+		// Integer[] UPC;
+		// UPC = new Integer[11];
+
+
+		// for(int i = 0; i<UPC.length; ++i)
+		// {
+		// 	UPC[i] = (int) (100.0 * Math.random()); 
+		// }
+		//
+
+		this.name = name;
+		this.price = price;
 	}
 
 	public int getstockNumber()
 	{	
-	
-		nextStockNumber = (int) (100000000000.0 * Math.random()); 
-
-		return nextStockNumber;
-
+		return '0' + nextStockNumber;
 	}
 	public int getPrice()
 	{
-		
+		return price;	
 	}
 	//methods
 	@Override
 	public String toString()
 	{
-
+		name = String.format("$-30s",name);
+		String priceFormatted = String.format("$%, 2f", price);
+		return name + priceFormatted;
 	}
 	//fields
 	private int nextStockNumber;
