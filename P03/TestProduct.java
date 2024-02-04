79c02c584a2ba9a4b6e5291957fae75c9eb48ed6
“Thomas <txa3582@mavs.uta.edu>
2024-02-04 14:11:26 -0600

    Update to TestProduct

--- P03/TestProduct.java
+++ P03/TestProduct.java
@@ -4,11 +4,22 @@ public class TestProduct
 	{
 
 		//Test1
-		
+		Product product1 = new Product("test name", 1111);
+		Product product2 = new Product("test name", 2222);
+		int testStock1 = product1.getstockNumber();	
+		int testStock2 = product2.getstockNumber();
+		if(testStock1 != 0)
+			System.err.println("FAIL: first stock number is not 0");
+
+		if(testStock2 != 1)
+			System.err.println("FAIL: second stock number is not 1");
 		//Test2
+		int testPrice = product1.getPrice();
+		if(testPrice != 1111)
+			System.err.println("FAIL: getPrice is not returning the correct the price");
 		//Test3
 
 		//Test4
-		if(Product.getPrice() < 0) throw IllegalArgumentException("");
+		// if(Product.getPrice() < 0) throw IllegalArgumentException("");
 	}
 }
