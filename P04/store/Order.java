b249bad27216d6a67a8457fd3628f866ab824cd8
“Thomas <txa3582@mavs.uta.edu>
2024-02-06 12:06:39 -0600

    order setup

---
+++ P04/store/Order.java
@@ -0,0 +1,29 @@
+package store;
+
+public class Order 
+{
+    public Order(Customer customer)
+    {
+
+    }
+    //methods
+    public addItem(Item item)
+    {
+        
+    }
+    getPrice()
+    {
+        
+    }
+    @Override
+    public String toString() 
+    {
+        
+    }
+
+    //fields
+    private static int nextOrderNumber;
+    private Item item[];
+    private Customer customer;
+
+}
