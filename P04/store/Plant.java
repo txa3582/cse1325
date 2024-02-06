bfff584a329858ced4347074efabc2a3b1b788be
“Thomas <txa3582@mavs.uta.edu>
2024-02-06 12:41:09 -0600

    Tool update

--- P04/store/Plant.java
+++ P04/store/Plant.java
@@ -1,3 +1,5 @@
+package store;
+
 public class Plant extends Product
 {
 
---
+++ P04/store/Tool.java
@@ -0,0 +1,10 @@
+package store;
+
+public class Tool extends Product
+{
+    public Tool(String name, double price)
+    {
+        super(name);
+        super(price);
+    }
+}
