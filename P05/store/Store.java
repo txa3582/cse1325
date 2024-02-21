c848a3ad81c7856435a4d1a7fb31f5d8df499188
“Thomas <txa3582@mavs.uta.edu>
2024-02-21 16:17:25 -0600

    Store setup

--- P05/store/Store.java
+++ P05/store/Store.java
@@ -1,1 +1,50 @@
 package store;
+
+import java.util.ArrayList;
+
+public class Store
+{
+    public Store(String name)
+    {
+
+    }
+    public String getName()
+    {
+        
+    }
+
+    public addCustomer(Customer customer)
+    {
+        return customer;
+    }
+    public getCustomerList()
+    {
+
+    }
+    public getProduct(Product product)
+    {
+
+    }
+    public String getProductList()
+    {
+
+    }
+    public int newOrder(int customer)
+    {
+
+    }
+    public addToOrder(int order, int product, int quantity)
+    {
+
+    }
+    public String getOrderList()
+    {
+        
+    }
+
+    // fields
+    private String name;
+    private ArrayList<Customer[]> customers;
+    private ArrayList<Product[]> products;
+    private ArrayList<Order[]> orders;
+}
