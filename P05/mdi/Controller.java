3c8aaf39c7a9a1fdbdecd971ebfcf6e02618809d
“Thomas <txa3582@mavs.uta.edu>
2024-02-21 12:21:01 -0600

    controller setup

---
+++ P05/mdi/Controller.java
@@ -0,0 +1,49 @@
+import store.;
+public class Controller
+{
+    public Controller(String storeName)
+    {
+
+    }
+    public void mdi()
+    {
+        while(isRunning == true)
+        {
+            
+        }
+    }
+
+    private exit()
+    {
+
+    }
+    private placeOrder()
+    {
+
+    }
+    private newCustomer()
+    {
+
+    }
+    private newTool()
+    {
+
+    }
+    private newPlant()
+    {
+
+    }
+    private switchView()
+    {
+
+    }
+    //fields
+    private Store store;
+    private View view;
+    private Menu mainMenu;
+    private String output;
+    private boolean isRunning;
+    private Scanner in;
+
+
+}
---
+++ P05/mdi/Manga.java
@@ -0,0 +1,8 @@
+public class Manga
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
+
