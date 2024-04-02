a3f1683987df87a420dbbd3c9353a9c5e051feac
“Thomas <txa3582@mavs.uta.edu>
2024-04-01 23:29:23 -0500

    last update

--- P08/extreme_bonus/odds.cpp
+++ P08/extreme_bonus/odds.cpp
@@ -3,14 +3,15 @@
 #include <string>
 #include <string.h>
 #include <algorithm>
-
+#include <random> 
+#include <chrono>  
 
 int main(int argc, char* argv[])
 {
     
     std::vector<std::string> odds;
     std::vector<std::string>* evens = new std::vector<std::string>;
-
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     int len = 0;
     std::string word;  
     for (int i = 1; i < argc; i++)
@@ -29,16 +30,18 @@ int main(int argc, char* argv[])
         
     }
     std::sort(odds.begin(), odds.end());
-    std::sort()
 
-    std::cout << "Odd lengths(Sorted): "<< std::endl;
 
+    std::cout << "Odd lengths(Sorted): \n"<< std::endl;
+
+
+    std::shuffle(evens->begin(), evens->end(), std::default_random_engine(seed));
     for (int i = 0; i < odds.size() ; i++)
     {
         std::cout << odds[i] << std::endl;
     }
     std::cout << "\n"<< std::endl;
-    std::cout << "Even lengths(Sorted): "<< std::endl;
+    std::cout << "Even lengths(Sorted): \n"<< std::endl;
     for (int i = 0; i < evens->size(); i++)
     {
         std::cout << (*evens)[i] << std::endl;
