e5fdd7ebb885dcd2c46c8be303d0ea256a4e492d
“Thomas <txa3582@mavs.uta.edu>
2024-04-08 21:58:58 -0500

    solver update

--- P09/full_credit/solver.cpp
+++ P09/full_credit/solver.cpp
@@ -1,23 +1,42 @@
 #include "linear.h"
+#include "quadratic.h"
 
 int main(int argc, char* argv[])
 {
-    if(argc != 3 || argc != 4)
+    if((argc < 3) || (argc > 4))
     {
         std::cerr << "Invalid polynomial size - must be linear or quadratic" << std::endl;
         exit(-1);
     }
-    std::vector<double> linearv = {1.0,2.0};
-    Polynomial* p = new Linear(linearv);
 
-    // if(argc == 3)
-    // {
+    Polynomial* p;
+
+    p->solve();
+    if(argc == 3)
+    {
+        double c1 = std::stod(argv[1]);
+        double c2 = std::stod(argv[2]);
+        std::vector<double> linearCoefficents = {c1,c2};
         
-    // }
-    // if(argc == 4)
-    // {
+        p = new Linear(linearCoefficents);
+    }
+    if(argc == 4)
+    {
+        double c1 = std::stod(argv[1]);
+        double c2 = std::stod(argv[2]);
+        double c3 = std::stod(argv[3]);
+        std::vector<double> QuadraticCoefficents = {c1,c2,c3};
         
-    // }
+        p = new Quadratic(QuadraticCoefficents);
+    }
+
+    std::vector<double> roots = p->solve();
+    for (int i: roots)
+    {
+        std::cout << i << std::endl;
+    }
+    
+    delete p;
 
     // Fix destructors
     return 0;
