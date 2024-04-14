f0e4e1cc0dd82cefa5977038fd11666acfe9dcae
“Thomas <txa3582@mavs.uta.edu>
2024-04-14 16:14:51 -0500

    ostream

---
+++ P10/full_credit/inch.cpp
@@ -0,0 +1,31 @@
+#include "inch.h"
+
+
+Inch::Inch(int whole, int numerator, int denominator)
+:   _whole{whole}, _numerator{numerator}, _denominator{denominator}
+{
+    validate();
+}
+
+Inch::Inch() : Inch(0,0,2){}
+
+Inch Inch::operator+(const Inch& rhs)
+{
+    int numerator = ((rhs._numerator * 64)/rhs._denominator) + ((this->_numerator * 64)/this->_denominator);
+    int whole = rhs._whole + this->_whole;
+
+    
+    Inch* resultptr = new Inch(whole,numerator,64);
+    Inch result = *resultptr;
+    return result;
+}
+
+std::ostream& operator<<(std::ostream& ost, const Inch& rhs)
+{
+    ost << rhs._whole, rhs._numerator/rhs._denominator;
+    return ost;
+}
+
+
+
+
