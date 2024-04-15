543cbce5fa1cef47de65fddd7e3d18a82f357cd6
“Thomas <txa3582@mavs.uta.edu>
2024-04-14 19:32:05 -0500

    validate

--- P10/full_credit/inch.cpp
+++ P10/full_credit/inch.cpp
@@ -1,5 +1,5 @@
 #include "inch.h"
-
+#include <numeric>
 
 Inch::Inch(int whole, int numerator, int denominator)
 :   _whole{whole}, _numerator{numerator}, _denominator{denominator}
@@ -38,11 +38,29 @@ std::istream& operator>>(std::istream& ist, Inch& inch)
 
 const int Inch::compare(const Inch& rhs)
 {
-    double inch1 = static_cast<double>(*this);
-    double inch2 = static_cast<double>(rhs);
+    // std::<double> inch1 = static_cast<double>(*this);
+    // double inch2 = static_cast<double>(rhs);
 
-    if (inch1 < inch2) return -1;
-    if (inch1 > inch2) return 1;
-    return 0;
+    // if (inch1 < inch2) return -1;
+    // if (inch1 > inch2) return 1;
+    // return 0;
 }
 
+void Inch::validate()
+{
+    if (_denominator != 2 && _denominator != 4 && _denominator != 8 && _denominator != 16 && _denominator != 32 && _denominator != 64)
+    {
+        throw std::invalid_argument("Denominator must be 2, 4, 8, 16, 32, or 64");
+    }
+    
+    while (_numerator > _denominator)
+    {
+        _numerator -= _denominator;
+    }
+
+    int GCD = std::gcd(_numerator, _denominator);
+
+    _numerator /= GCD;
+    _denominator /= GCD;
+
+}
