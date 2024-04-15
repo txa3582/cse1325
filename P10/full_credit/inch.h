fd4daeda6c6bdc98b4e89dcd84af18376bc6c35c
“Thomas <txa3582@mavs.uta.edu>
2024-04-14 19:38:13 -0500

    fix

---
+++ P10/full_credit/inch.h
@@ -0,0 +1,32 @@
+#ifndef __INCH_H
+#define __INCH_H
+
+#include <iostream>
+class Inch
+{
+    private:
+        int _whole;
+        int _numerator;
+        int _denominator;
+        
+    public:
+        Inch();
+        Inch(int whole, int numerator, int denominator);
+        Inch operator+(const Inch& rhs);
+    
+        inline bool operator==(const Inch& rhs){return (compare(rhs) == 0);}
+        inline bool operator!=(const Inch& rhs){return (compare(rhs) != 0);}
+        inline bool operator<(const Inch& rhs){return (compare(rhs) < 0);}
+        inline bool operator<=(const Inch& rhs){return (compare(rhs) <= 0);}
+        inline bool operator>(const Inch& rhs){return (compare(rhs) > 0);}
+        inline bool operator>=(const Inch& rhs){return (compare(rhs) >= 0);}
+
+        friend std::ostream& operator<<(std::ostream& ost, const Inch& rhs);
+        friend std::istream& operator>>(std::istream& ist, const Inch& rhs);
+
+    private:
+        const int compare(const Inch& rhs);
+        void validate();
+};
+
+#endif
