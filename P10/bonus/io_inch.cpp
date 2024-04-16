b65f68c23294036bb6013631223e89cd2ff07a33
“Thomas <txa3582@mavs.uta.edu>
2024-04-15 23:06:53 -0500

    finished bonus

--- P10/bonus/io_inch.cpp
+++ P10/bonus/io_inch.cpp
@@ -1,34 +1,58 @@
 #include "inch.h"
 #include <fstream>
+#include <string>
+#include <sstream>
+
 Inch in, last_in, sum;
 
 
 int main(int argc, char* argv[])
 {
-
+    // read file
     std::ifstream ist{std::string{argv[1]}};
     if(!ist) throw std::runtime_error{"can't open input file"};
 
-    std::string s;
-    while(std::getline(ist, s)) std::cout << s << std::endl;
-    
+
+
     // write file
     std::ofstream ofs {std::string{argv[2]}};
     if(!ofs) throw std::runtime_error{"can't open output file"};
-    ofs << "Writing this to " << argv[2] <<std::endl;
 
-    std::cout << "Starting sum is " << sum <<", last entry was " << last_in;
+    
+    std::cout << "Starting sum is " << sum <<", last entry was " << last_in << std::endl;
+    std::cout << "Writing inputs to " << argv[2] << std::endl;
+    std::string s;
+
+    while (std::getline(ist, s))
+    {   
+        ofs << s << std::endl;
+        std::cout << "\n" << std::endl;
+        std::cout << "Read " << s << " from " << argv[1] << std::endl;
+        std::istringstream iss(s);
+        iss >> in;
+        std::cout << "\n" << std::endl;
+        sum = sum + in;
+        if(in == last_in) std::cout << "Same legth as last time!" << std::endl;
+        if(in != last_in) std::cout << "NOT the same length as last time." << std::endl;
+        if(in <  last_in) std::cout << "Smaller than the length last time." << std::endl;
+        if(in <= last_in) std::cout << "Smaller or same length as last time." << std::endl;
+        if(in >  last_in) std::cout << "Bigger than the length last time." << std::endl;
+        if(in >= last_in) std::cout << "Bigger or same length as last time." << std::endl;
+
+        std::cout << "Sum so far is " << sum << std::endl;
+        std::cout << "\n" << std::endl;
+
+        last_in = in;
+    }
+  
     while (std::cin)
     {
-        // read file
 
-
-
-        std::cout << "\n" << std::endl;
         std::cout << "Enter one or more measurements (in inches): ";
         std::cin >> in;
         std::cout << "\n" << std::endl;
         std::cout << "You entered  " << in;
+        ofs << in << std::endl;
         sum = sum + in;
         std::cout << "\n" << std::endl;
 
@@ -45,6 +69,8 @@ int main(int argc, char* argv[])
         last_in = in;
     }
     
+    ist.close();
+    ofs.close();
     
 
     
