a6a3d602f90ec231a1c082049cfc731a409f666b
“Thomas <txa3582@mavs.uta.edu>
2024-04-17 16:08:09 -0500

    header file

--- P11/full_credit/university.h
+++ P11/full_credit/university.h
@@ -1,7 +1,22 @@
 #ifndef __UNIVERSITY_H
 #define __UNIVERSITY_H
+#include <iostream>
+class University
+{
+    private:
+        std::string _name;
+        int enrollment;
 
+    public:
+        University(std::string _name, int _enrollment);
+        const std::string name();
+        const int enrollment();
 
+        friend std::istream& operator>>(std::istream& ist, University& reading);
+        friend std::ostream& operator<<(std::ostream& ist, const University& reading);
+    private:
+        void validate();
+};
 
 
 #endif
